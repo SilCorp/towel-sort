159e1aeb0b95a4f7a56d15199f950e308aa1d55e
Alexander Silchenko <san4ys.1998@gmail.com>
2020-09-29 22:11:36 +0300

    feat: solve "Towel Sort" task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix) return [];
+
+    return matrix.map( (arr, index) => {
+        const isOdd = !(index % 2);
+        return isOdd ?
+            arr.sort( (a, b) => a - b ) :
+            arr.sort( (a, b) => b - a );
+    }).flat();
+};
